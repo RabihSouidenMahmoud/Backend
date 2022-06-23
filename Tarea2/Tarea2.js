7401a63fd5c1385be35c1bf575ec9b0019d2baa0
RabihSouidenMahmoud <86632928+RabihSouidenMahmoud@users.noreply.github.com>
2022-06-22 22:04:49 -0500

    Add files via upload

---
+++ Tarea2/Tarea2.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+
+class Contenedor {
+    constructor(archivo){
+        this.archivo = archivo
+        this.objects = JSON.parse(fs.readFileSync(this.archivo,'utf-8'))
+    }
+
+    save(object){
+        if(this.objects.length>0){
+            object.id = this.objects.at(-1).id + 1 
+        }else{
+            object.id = 1
+        }   
+        this.objects.push(object)
+        fs.writeFileSync(this.archivo, JSON.stringify(this.objects))
+        return(object.id)
+    }
+
+    getById(id){
+        return this.objects.find( object => object.id === id)
+    }
+
+    getAll(){
+        return this.objects
+    }
+
+    deleteById(id){
+        const index = this.objects.findIndex( object => object.id === id)
+        this.objects.splice(index, 1)
+        fs.writeFileSync(this.archivo, JSON.stringify(this.objects))
+    }
+
+    deleteAll(){
+        this.objects = []
+        fs.writeFileSync(this.archivo, JSON.stringify(this.objects))
+    }
+
+}
+
+const contenedor = new Contenedor('Productos.txt') 
+console.log(contenedor.objects)
+console.log(contenedor.save({                                                                                                                                                    
+    "title": "Test",                                                                                                                              
+    "price": 111,                                                                                                                                     
+    "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",                                          
+    "id": 0                                                                                                                                              
+  }))
+console.log(contenedor.getById(2))
+console.log(contenedor.getAll())
+contenedor.deleteById(3)
+console.log(contenedor.objects)
+contenedor.deleteAll()
+console.log(contenedor.objects)
